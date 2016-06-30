dd2a3e2c572afb82c88d5d9eed0389ba61321768
ied206 <jb6804@naver.com>
2016-06-30 18:50:27 +0900

    Finish ELF header parsing

--- elf.py
+++ elf.py
@@ -6,23 +6,30 @@ import os
 import struct
 from ctypes import *
 
-ELF_ENDIAN_LITTLE = 1
-ELF_ENDIAN_BIG = 2
-ELF_FILETYPE_EXEC = 2
-ELF_FILETYPE_SHARED = 3
-ELF_MACHINE_IA32 = 0x03
-ELF_MACHINE_AMD64 = 0x3E
+ELF_e_endian_LITTLE = 1
+ELF_e_endian_BIG = 2
+ELF_e_type_EXEC = 2
+ELF_e_type_SHARED = 3
+ELF_e_machine_IA32 = 0x03
+ELF_e_machine_AMD64 = 0x3E
 
 class ElfCtx:
     filename = ''
-    data = '' # self.data's type is str
-    bitness = 0
-    endian = 0
-    filetype = 0
-    machine = 0
-    entrypoint = 0
-    phdroff = 0
-    shdroff = 0
+    data = ''  # self.data's type is str
+    # ELF Header
+    e_class = 0
+    e_endian = 0
+    e_type = 0
+    e_machine = 0
+    e_entry = 0
+    e_phoff = 0
+    e_shoff = 0
+    e_ehsize = 0
+    e_phentsize = 0
+    e_phnum = 0
+    e_shentsize = 0
+    e_shnum = 0
+    e_shstrndx = 0
 
     def __init__(self, filename):
         # read from file
@@ -34,68 +41,77 @@ class ElfCtx:
         if not (self.data[0x00:0x04] == "\x7F\x45\x4C\x46"):
             print "[ERR] {0} is not ELF File!".format(self.filename)
             exit(1)
-        # get ELF's bitness
+        # get ELF's e_class
         if (self.data[0x04] == "\x01"):
-            self.bitness = 32
+            self.e_class = 32
             print "[ERR] This does not support ELF32"
             exit(1)
         elif (self.data[0x04] == "\x02"):
-            self.bitness = 64
+            self.e_class = 64
         else:
-            print "[ERR] Wrong ELF bitness : {0}".format(ord(self.data[4]))
+            print "[ERR] Wrong ELF e_class : {0}".format(ord(self.data[4]))
             exit(1)
-        # get endian
+        # get e_endian
         if (self.data[0x05] == "\x01"):
-            self.endian = ELF_ENDIAN_LITTLE
+            self.e_endian = ELF_e_endian_LITTLE
         elif (self.data[0x05] == "\x02"):
-            self.endian = ELF_ENDIAN_BIG
-            print "[ERR] Do not support BIG ENDIAN"
+            self.e_endian = ELF_e_endian_BIG
+            print "[ERR] Do not support BIG e_endian"
             exit(1)
         else:
-            print "[ERR] Wrong ELF endian : {0}".format(ord(self.data[0x05]))
+            print "[ERR] Wrong ELF e_endian : {0}".format(ord(self.data[0x05]))
             exit(1)
         # read ELF file type
         if (self.data[0x10] == "\x02"):
-            self.filetype = ELF_FILETYPE_EXEC
+            self.e_type = ELF_e_type_EXEC
         elif (self.data[0x10] == "\x03"):
-            self.filetype = ELF_FILETYPE_SHARED
+            self.e_type = ELF_e_type_SHARED
         else:
             print "[ERR] Wrong ELF type : {0}".format(ord(self.data[0x10]))
             exit(1)
-        # read ELF machine type
+        # read ELF e_machine type
         if (self.data[0x12] == "\x03"):
-            self.machine = ELF_MACHINE_IA32
+            self.e_machine = ELF_e_machine_IA32
             print "[ERR] This does not support IA32"
             exit(1)
         elif (self.data[0x12] == "\x3E"):
-            self.machine = ELF_MACHINE_AMD64
+            self.e_machine = ELF_e_machine_AMD64
         else:
             print "[ERR] Wrong ELF arch : {0}".format(ord(self.data[0x12]))
             exit(1)
         # read entry point
-        self.entrypoint = c_void_p(struct.unpack("<Q", self.data[0x18:0x20])[0])
-        # program header table
-        self.phdroff = c_void_p(struct.unpack("<Q", self.data[0x20:0x28])[0])
-        # section header table
-        self.shdroff = c_void_p(struct.unpack("<Q", self.data[0x28:0x30])[0])
+        self.e_entry = c_void_p(struct.unpack("<Q", self.data[0x18:0x20])[0])
+        # program header table offset
+        self.e_phoff = c_void_p(struct.unpack("<Q", self.data[0x20:0x28])[0])
+        # section header table offset
+        self.e_shoff = c_void_p(struct.unpack("<Q", self.data[0x28:0x30])[0])
+        # size of this ELF header
+        self.e_ehsize = struct.unpack("<H", self.data[0x34:0x36])[0]
+        # info about program header table
+        self.e_phentsize = struct.unpack("<H", self.data[0x36:0x38])[0]
+        self.e_phnum = struct.unpack("<H", self.data[0x38:0x3A])[0]
+        # info about section header table
+        self.e_shentsize = struct.unpack("<H", self.data[0x3A:0x3C])[0]
+        self.e_shnum = struct.unpack("<H", self.data[0x3C:0x3E])[0]
+        self.e_shstrndx = struct.unpack("<H", self.data[0x3E:0x40])[0]
 
         # currently this only support amd64
 
     def print_elf_info(self):
         tmp = ''
         print "Filename    : {0}".format(self.filename)
-        print "Bitness     : {0}".format(self.bitness)
-        if self.endian == ELF_ENDIAN_LITTLE:
-            tmp = 'Little Endian'
-        print "Endian      : {0}".format(tmp)
+        print "e_class     : {0}".format(self.e_class)
+        if self.e_endian == ELF_e_endian_LITTLE:
+            tmp = 'Little e_endian'
+        print "e_endian      : {0}".format(tmp)
         tmp = ''
-        if self.filetype == ELF_FILETYPE_EXEC:
+        if self.e_type == ELF_e_type_EXEC:
             tmp = 'Executable'
-        elif self.filetype == ELF_FILETYPE_SHARED:
+        elif self.e_type == ELF_e_type_SHARED:
             tmp = 'Shared'
         print "Type        : {0}".format(tmp)
         tmp = ''
-        if self.machine == ELF_MACHINE_AMD64:
+        if self.e_machine == ELF_e_machine_AMD64:
             tmp = 'AMD64'
-        print "Machine     : {0}".format(tmp)
-        print "EntryPorint : 0x%012X" % (self.entrypoint.value)
+        print "e_machine     : {0}".format(tmp)
+        print "EntryPorint : 0x%012X" % (self.e_entry.value)
